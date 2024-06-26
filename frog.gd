ef9d9773d32f150f8748b9b229372e697257eb78
Howard Akers <howard.akers.iii@gmail.com>
2024-06-26 11:46:48 -0400

    Implemented basic chase mechanic

--- frog.gd
+++ frog.gd
@@ -3,23 +3,43 @@
 #	Frog Stuff
 #
 #	Author CheeriestTomcat
-#	Created 6/25/24
+#	Created 6/26/24
 #   Last Modified 6/25/24
 #
 #
 #**************************************************************************************
-
-
 extends CharacterBody2D
+
+var player
 
 #This implements gravity for frog
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#This is the chase variable
+var chase = false
+#Set frog speed
+var SPEED = 50
 func _physics_process(delta):
-	
+	#This does the fall speed
 	velocity.y += gravity * delta
+	#This finds the player
+	player = get_node("../../Player/Player")
+	var direction = (player.position - self.position).normalized()
+	#Chase the player
+	if chase == true:
+		if direction.x < 0:
+			get_node("AnimatedSprite2D").flip_h = false
+			#print("Chase Left")
+		else:
+			get_node("AnimatedSprite2D").flip_h = true
+			#print("Chase Right")
+		velocity.x = direction.x * SPEED
 	#This makes the gravity n stuff work
 	move_and_slide()
+	
 #This is the character detection
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
-		print("Player Detected")
+		chase = true
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
