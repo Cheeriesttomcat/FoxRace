40531c76bc54e54a4c619a28d387ffea2549f5e1
Howard Akers <howard.akers.iii@gmail.com>
2024-07-02 12:47:07 -0400

    eagle now doesn't blow up

---
+++ eagle.gd
@@ -0,0 +1,69 @@
+#**************************************************************************************
+#
+#	Eagle Stuff
+#	Author CheeriestTomcat
+#	Created 7/2/24
+#   Last Modified 7/2/24
+#
+#
+#**************************************************************************************
+extends CharacterBody2D
+
+var player
+
+#This implements gravity for frog
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#This is the chase variable
+#var chase = false
+#Set frog speed
+var SPEED = 100
+#var HOPS = -300
+#reset idle
+func _ready():
+	get_node("AnimatedSprite2D").play("default")
+	
+func _physics_process(delta):
+	#This finds the player
+	player = get_node("../../Player/Player")
+	var direction = (player.position - self.position).normalized()
+	if direction.x < 0:
+		get_node("AnimatedSprite2D").flip_h = false
+		#print("Chase Left")
+	else:
+		get_node("AnimatedSprite2D").flip_h = true
+		#print("Chase Right")
+
+	if get_node("AnimatedSprite2D").animation != "death":
+		if self.is_on_floor():
+			get_node("AnimatedSprite2D").play("jump")
+			velocity.x = SPEED * direction.x
+	else:
+		velocity.x = 0
+
+
+
+	#This makes the gravity n stuff work
+	move_and_slide()
+
+
+#Kill a frog
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		body.velocity.y = -400
+		death()
+
+#Hurt the player
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		if get_node("AnimatedSprite2D").animation != "death":
+			Game.PlayerHp -= 3
+			death()
+
+#Do the stuff
+func death():
+		Game.Gold += 5
+		Utils.saveGame()
+		$owsound.play()
+		get_node("AnimatedSprite2D").play("death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
