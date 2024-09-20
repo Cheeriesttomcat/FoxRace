19b43b823f6dfac7ee49a0c385c51bf64b3fa288
Howard Akers <howard.akers.iii@gmail.com>
2024-09-20 17:01:36 -0400

    climb modifications

--- player/player.gd
+++ player/player.gd
@@ -20,7 +20,7 @@ const OWW = 300
 var pain = false
 #ladder checker
 var on_ladder := false
-
+var climbin := false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -28,39 +28,55 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Player health
 #var health = 10 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#Deal with character impact
 	if pain == true:
 		$PainTimer.one_shot = true
 		$PainTimer.start(PAINTIME)
 		pain = false
+		climbin = false
 		anim.play("hurt")
-	elif $PainTimer.is_stopped():
+
+	# Add the gravity.
+	if !climbin and !is_on_floor():
+		velocity.y += gravity * delta
+
+	if $PainTimer.is_stopped():
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or climbin):
 			#get_node("AnimatedSprite2D").animation != "hurt":
 			anim.play("jump")
 			velocity.y = JUMP_VELOCITY
+			climbin = false
 			$Boing.play()
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("ui_left", "ui_right")
-		var up = Input.is_action_pressed("ui_up")
 		var down = Input.is_action_pressed("ui_down")
+		var up = Input.is_action_pressed("ui_up")
 		if direction == -1:
 			get_node("AnimatedSprite2D").flip_h = true
 		elif direction == 1:
 			get_node("AnimatedSprite2D").flip_h = false
 		if direction:
 			#if get_node("AnimatedSprite2D").animation != "hurt":
-			if !down:
-				velocity.x = direction * SPEED
-				if velocity.y == 0:
-					anim.play("run")
+			velocity.x = direction * SPEED
+			if velocity.y == 0:
+				anim.play("run")
+			climbin = false
+#			else:
+#				velocity.x = move_toward(velocity.x, 0, SPEED)
+#				anim.play("crouch")
+		elif on_ladder and (up or down):
+			climbin = true
+			if up:
+				velocity.y = -SPEED * .5
+				anim.play("climb")
 			else:
-				velocity.x = move_toward(velocity.x, 0, SPEED)
-				anim.play("crouch")
+				velocity.y = SPEED * .5
+				anim.play_backwards("climb")
+		elif climbin:
+			velocity.y = 0
+			anim.pause()
 		else:
 			#if get_node("AnimatedSprite2D").animation != "hurt":
 			velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -70,7 +86,7 @@ func _physics_process(delta):
 					anim.play("idle")
 				else:
 					anim.play("crouch")
-		if velocity.y > 0:
+		if (velocity.y > 0) and !climbin:
 		# get_node("AnimatedSprite2D").animation != "hurt":
 			anim.play("fall")
 	move_and_slide()
@@ -80,3 +96,12 @@ func _physics_process(delta):
 		queue_free()
 		get_tree().change_scene_to_file("res://main.tscn")
 		
+
+#Ladder Checker
+func _on_ladder_checker_body_entered(body):
+	on_ladder = true
+	#print("On ladder")
+
+func _on_ladder_checker_body_exited(body):
+	on_ladder = false
+	#print("Off ladder")
