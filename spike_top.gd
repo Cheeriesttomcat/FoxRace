161821e509b009d7dd5379f79b2d2da34e116576
Howard Akers <howard.akers.iii@gmail.com>
2024-09-23 13:24:07 -0400

    Top Spike Added

---
+++ spike_top.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+#**************************************************************************************
+#
+#	Spikes with skull-- OW
+#	Author CheeriestTomcat
+#	Created 9/23/24
+#   Last Modified 9/23/24
+#
+#
+#**************************************************************************************
+
+var player
+const IMPACT = 3
+func _ready():
+	player = get_node("../../Player/Player")
+
+func _on_player_owie_body_entered(body):
+	if body.name == "Player":
+		if body.get_node("AnimatedSprite2D").animation != "hurt":
+			Game.PlayerHp -= IMPACT
+			if (player.position.x - self.position.x) < 0:
+				body.velocity.x = -1.0 * body.OWW
+			else:
+				body.velocity.x = body.OWW
+			body.velocity.y = -1 * body.OWW
+			body.pain = true
